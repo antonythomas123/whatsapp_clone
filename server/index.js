fd103439c27af20708ab01fdcefd5b4598a55543
antonythomas123 <antonythomas993@gmail.com>
2023-09-01 11:37:15 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 3500;
+
+app.listen(PORT, ()=> {
+    console.log(`Server listening on port ${PORT}`);
+});
+
